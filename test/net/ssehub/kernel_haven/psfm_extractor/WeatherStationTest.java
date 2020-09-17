36d372113f5dfb8465772ecb4ea5074d8a15dc38
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-09-17 10:02:46 +0200

    Tests based on WeatherStation example

--- test/net/ssehub/kernel_haven/psfm_extractor/WeatherStationTest.java
+++ test/net/ssehub/kernel_haven/psfm_extractor/WeatherStationTest.java
@@ -15,22 +15,81 @@
  */
 package net.ssehub.kernel_haven.psfm_extractor;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import net.ssehub.kernel_haven.variability_model.VariabilityModel;
 
 /**
  * Tests the PsFmExtractor for the atomic feature models.
  *
- * @author Calvin Hansch
+ * @author Sascha El-Sharkawy
  */
-public class WeatherStationTest {
+@RunWith(Parameterized.class)
+public class WeatherStationTest extends AbstractModelTest {
+    private static final VariabilityModel MODEL = loadModel(new File("testdata/xfm/WeatherStation.xfm"));
+    
+    private String variableName;
+    private String type;
+    private int nReferencedVars;
+    private int nReferncesToVar;
+    private int nestingDepth;
+
+    /**
+     * Constructor used by {@link #data()} method.
+     * @param variableName The expected name of the variable.
+     * @param type The expected type of the variable
+     * @param nReferencedVars The expected number of other variables referenced by the tested variable
+     * @param nReferncesToVar The expected number of variables that reference the tested variable
+     * @param nestingDepth The expected nesting depth of the variable
+     */
+    public WeatherStationTest(String variableName, String type, int nReferencedVars, int nReferncesToVar,
+        int nestingDepth) {
+        
+        this.variableName = variableName;
+        this.type = type;
+        this.nReferencedVars = nReferencedVars;
+        this.nReferncesToVar = nReferncesToVar;
+        this.nestingDepth = nestingDepth;
+    }
     
     /**
-     * Test with the WeatherStation.xfm feature model.
+     * Provides the test data to be tested.
+     * @return Each lines represents one test
+     */
+    @Parameters(name = "{index}: {0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            // The root
+            {"WeatherStation", "mandatory", 0, 0, 0},
+            // Sensors
+            {"Sensors"    , "mandatory", 0, 0, 1},
+            {"Temperature", "or"       , 0, 1, 2},
+            {"Color"      , "optional" , 0, 0, 3},
+            {"WindSpeed"  , "or"       , 0, 1, 2},
+            {"AirPressure", "or"       , 0, 0, 2},
+            // Languages
+            {"Languages", "mandatory"  , 0, 0, 1},
+            {"English"  , "alternative", 0, 0, 2},
+            {"German"   , "alternative", 0, 0, 2},
+            // Warnings
+            {"Warnings", "optional", 0, 0, 1},
+            {"Gale"    , "or"      , 1, 0, 2},
+            {"Heat"    , "or"      , 1, 0, 2},
+        });
+    }
+    
+    /**
+     * Performs the test.
      */
     @Test
-    public void testWS() {
-        fail("Not yet implemented");
+    public void test() {
+        assertVariableInModel(MODEL, variableName, type, nReferencedVars, nReferncesToVar, nestingDepth);
     }
 }
